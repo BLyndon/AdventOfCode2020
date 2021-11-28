91080ad1ba91e34f4a1d8716b6f561da6654d493
BLyndon <47363948+BLyndon@users.noreply.github.com>
2021-11-28 12:42:53 +0100

    update parser for part 2

--- day_07.py
+++ day_07.py
@@ -9,30 +9,35 @@ def loader(filename):
 
 
 def parser(lines):
+    def expand_bags(bags):
+        edges = set()
+        for bag, content in bags.items():
+            bag = [bag]
+            for inner_bag in content:
+                edges.add(tuple(bag + inner_bag))
+        return list(edges)
+
     bags = dict()
     for line in lines:
-        line = line.replace("\n", "")
+        line = line.replace("\n", "").replace("bags", "").replace(
+            "bag", "")
         if line.endswith("no other bags."):
             content = ['no bags']
         else:
-            line = line.replace("bags", "").replace(
-                "bag", "").replace(' , ', "").replace(".", "").strip()
+            line = line.replace(' , ', "-").replace(" .", "").strip()
 
-            content = line.split("contain")[1]
-            content = re.split(r'\d ', content)[1:]
+            content = line.split("contain ")[1].split("-")
+            content = list(map(lambda x: [
+                " ".join(x.split()[1:]), x.split()[0]], content))
         bag = line.split("contain")[0].strip()
 
         bags[bag] = content
 
-    edges = set()
-    for bag, content in bags.items():
-        for inner_bag in content:
-            edges.add((bag, inner_bag))
-
-    return list(edges)
+    return expand_bags(bags)
 
 
 def solver_1(edges):
+    edges = [(edge[0], edge[1]) for edge in edges]
     G = nx.DiGraph()
     G.add_edges_from(edges)
 
